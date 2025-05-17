efa6bda033dca4e94e5ead958c4df78dfbfe79e8
Nathanael Lambert <nathanael.lambert@protonmail.com>
2025-05-17 17:19:10 +0200

    lerp

--- earbrowsing/js/LineOfSight.js
+++ earbrowsing/js/LineOfSight.js
@@ -1,8 +1,14 @@
-// js/LineOfSight.js
-export class LineOfSight {
+// Helper function for shortest angle interpolation
+function lerpAngle(a, b, t) {
+    let diff = b - a;
+    while (diff < -Math.PI) diff += 2 * Math.PI;
+    while (diff > Math.PI) diff -= 2 * Math.PI;
+    return a + diff * t;
+  }
+  
+  class LineOfSight {
     constructor(audioSpace) {
       this.audioSpace = audioSpace;
-      // Start with initial values
       this.display_x = audioSpace.finger_x;
       this.display_y = audioSpace.finger_y;
       this.display_direction = audioSpace.direction_facing;
@@ -13,21 +19,17 @@ export class LineOfSight {
     }
   
     update({ finger_x, finger_y, direction_facing }) {
-      // Set the new target position and direction
       this.target_x = finger_x;
       this.target_y = finger_y;
       this.target_direction = direction_facing;
     }
   
     animate() {
-      // Easing factor (0.1 = slow, 1 = instant)
       const ease = 0.15;
-      // Move display position toward target
       this.display_x += (this.target_x - this.display_x) * ease;
       this.display_y += (this.target_y - this.display_y) * ease;
-      this.display_direction += (this.target_direction - this.display_direction) * ease;
+      this.display_direction = lerpAngle(this.display_direction, this.target_direction, ease);
   
-      // Draw the line
       const { ctx, width, height } = this.audioSpace;
       ctx.clearRect(0, 0, width, height);
       ctx.strokeStyle = "#00FFAA";
@@ -40,7 +42,6 @@ export class LineOfSight {
       ctx.lineTo(end_x, end_y);
       ctx.stroke();
   
-      // Continue animation
       requestAnimationFrame(() => this.animate());
     }
   }
